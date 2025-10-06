77d6cdbf49362d824c878229c8d32f55e05e2faf
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-10-06 22:47:24 +0000

    added ghcr support to docker

--- commands/docker.py
+++ commands/docker.py
@@ -1,6 +1,7 @@
 import os
 from commands import Executor
 from .utils import get_docker_image_name
+from .version import get_version
 
 class DockerStarter(Executor):
     
@@ -9,13 +10,15 @@ class DockerStarter(Executor):
                  image_folder: str,
                  experiment_name: str = 'default_experiment',
                  debug: bool = False,
-                 worm: bool = False):
+                 worm: bool = False,):
         self.debug = debug
         self.worm = worm
-        self.docker_image_name = get_docker_image_name(debug=self.debug, worm=self.worm)
+        self.version = get_version()
+        self.docker_image_name = f"ghcr.io/parsa-epfl/qflex:{get_docker_image_name(debug=self.debug, worm=self.worm)}-{self.version}"
         self.images_folder = './images'
         self.experiment_name = experiment_name
         self.image_folder = image_folder
+        print(f"============== Using QFlex version: {self.version} ==============")
 
     def cmd(self) -> str:
         cwd = os.getcwd()
@@ -45,29 +48,24 @@ class DockerStarter(Executor):
         # TODO remove unecessary mounts including .sh ones
         return f"""
         docker run -it --entrypoint /bin/bash \
-        -v {cwd}/images:/home/dev/qflex/images \
         -v {cwd}/cfg/{self.experiment_name}:/home/dev/qflex/cfg/{self.experiment_name} \
-        -v {cwd}/experiments:/home/dev/qflex/experiments \
-        -v {cwd}/typer_inputs:/home/dev/qflex/typer_inputs \
-        -v {cwd}/commands:/home/dev/qflex/commands \
-        -v {cwd}/build-multiple-kraken_vanilla.py:/home/dev/qflex/build-multiple-kraken_vanilla.py \
         -v {self.image_folder}:{self.image_folder} \
         -v {cwd}/sample_scripts:/home/dev/qflex/sample_scripts \
         -v {cwd}/QEMU_EFI.fd:/home/dev/qflex/QEMU_EFI.fd \
         -v {cwd}/templates:/home/dev/qflex/templates \
-        -v {cwd}/core_info.csv:/home/dev/qflex/core_info.csv \
-        -v {cwd}/WormCache:/home/dev/qflex/WormCache \
-        -v {cwd}/tmp_results:/home/dev/qflex/tmp_results \
+        -v {cwd}/typer_inputs:/home/dev/qflex/typer_inputs \
+        -v {cwd}/commands:/home/dev/qflex/commands \
         {commands_mount} {binary_mount} {self.docker_image_name}
         """
-        
+
 
 class DockerBuild(Executor):
     
     def __init__(self, 
                  debug: bool = False,
                  worm: bool = False,
-                 worm_only: bool = False):
+                 worm_only: bool = False,
+                 push: bool = False):
         self.debug = debug
         self.worm = worm
         if self.worm:
@@ -79,26 +77,54 @@ class DockerBuild(Executor):
         if self.debug:
             self.build_type = 'debug'
         self.worm_only = worm_only
+        self.push = push
+        self.version = get_version()
+        # TODO add checks to prevent overwriting existing images
+        print(f"============== Building QFlex version: {self.version} ==============")
 
     def cmd(self) -> str:
         
+        # TODO centeralize the ghcr.io/parsa-epfl/qflex part
         if not self.worm_only:
-            base_image_cmd = f"""
-            docker buildx build -t {self.docker_base_image_name}:latest --build-arg MODE={self.build_type} .
-            """
-        else:
-            base_image_cmd = f"echo 'Skipping base image build as --worm-only is set.'"
-
-        worm_image_cmd = f"""
-        docker buildx build -t {self.docker_image_name_with_worm}:latest --build-arg BASE_IMAGE={self.docker_base_image_name} -f Dockerfile.WormCache .
-        """
-        # TODO add a seperate debug image that has the files that can be used for compilation and developement without remaking the docker image
-        
-        if self.worm:
-            return [
-                base_image_cmd,
-                worm_image_cmd
+            base_image_build_cmd = [
+                f"""
+                docker buildx build -t {self.docker_base_image_name}:{self.version} --build-arg MODE={self.build_type} .
+                """,
+                f"docker tag {self.docker_base_image_name}:{self.version} ghcr.io/parsa-epfl/qflex:{self.docker_base_image_name}-{self.version}"
+            ]
+            base_image_push_cmd = [
+                f"docker push ghcr.io/parsa-epfl/qflex:{self.docker_base_image_name}-{self.version}"
             ]
         else:
-            return base_image_cmd
+            base_image_build_cmd = [
+                f"echo 'Skipping base image build as --worm-only is set.'"
+            ]
+            base_image_push_cmd = [
+                f"echo 'Skipping base image push as --worm-only is set.'"
+            ]
+
+        worm_image_cmd = [
+            f"""
+            docker buildx build -t {self.docker_image_name_with_worm}:{self.version} --build-arg BASE_IMAGE=ghcr.io/parsa-epfl/qflex:{self.docker_base_image_name}-latest -f Dockerfile.WormCache .
+            """,
+            f"docker tag {self.docker_image_name_with_worm}:{self.version} ghcr.io/parsa-epfl/qflex:{self.docker_image_name_with_worm}-{self.version}"
+        ]
+        # TODO add a seperate debug image that has the files that can be used for compilation and developement without remaking the docker image
+        worm_image_push_cmd = [
+            f"docker push ghcr.io/parsa-epfl/qflex:{self.docker_image_name_with_worm}-{self.version}"
+        ]
+
+        base_cmd = base_image_build_cmd
+        worm_cmd = worm_image_cmd
+        if self.push:
+            base_cmd += base_image_push_cmd
+            worm_cmd += worm_image_push_cmd
+
+
+
+        cmd = base_cmd
+        if self.worm:
+            cmd += worm_cmd
+
+        return cmd
 
