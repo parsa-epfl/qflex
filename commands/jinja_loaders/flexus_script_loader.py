1d742caccd97a9b43325eb525d8971b1f0ce1b33
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-10-10 01:06:35 +0000

    updated how run_flexus is created and fixed bugs

--- commands/jinja_loaders/flexus_script_loader.py
+++ commands/jinja_loaders/flexus_script_loader.py
@@ -1,5 +1,7 @@
+import os
 from .parameterloader import ParameterLoader
 from ..config import ExperimentContext
+
 
 class FlexusScriptLoader(ParameterLoader):
     def __init__(self,
@@ -10,11 +12,18 @@ class FlexusScriptLoader(ParameterLoader):
                 output_name='run_flexus.sh',
                 out_folder='scripts'
             )
+    
+    def load_parameters(self) -> str:
+        result = super().load_parameters()
+        # Make the script executable
+        os.system(f"chmod +x {result}")
+        return result
+
     def get_context(self):
         return {
             # TODO add some check to make sure this core_count is not the doubled version as that is being sent in as a separate parameter
             "CORE_COUNT": self.experiment_context.simulation_context.core_count,
             "DOUBLED_VCPU": self.experiment_context.simulation_context.doubled_vcpu,
-            "MEMORY": self.experiment_context.simulation_context.memory,
+            "MEMORY": self.experiment_context.simulation_context.memory_gb * 1024,  # in MB
             "QEMU_NIC": self.experiment_context.simulation_context.qemu_nic,
         }
