a5ed05dba73d1eae66251dc53bf662326ed6f3b4
pooriaPoorsarv <pooriapoorsarvi@gmail.com>
2025-10-16 18:02:28 +0200

    fixed image import for micro

--- micro_scripts/start_docker_3.2.1.sh
+++ micro_scripts/start_docker_3.2.1.sh
@@ -3,7 +3,7 @@ pushd ..
 parent=$(realpath $PWD/..)
 docker run -it --entrypoint /bin/bash \
     -v $parent/experiments/:home/dev/experiments \
-    -v $parent/../root.qcow2:home/dev/root.qcow2 \
+    -v $parent/root.qcow2:home/dev/root.qcow2 \
     -v $parent/qflex:home/dev/qflex \
     -w /home/dev/qflex/micro_scripts \
     --security-opt seccomp=unconfined \
