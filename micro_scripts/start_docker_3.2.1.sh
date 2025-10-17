841b8d86f28ebdc1cbfeb49e7d9c0accf12c35e6
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-10-17 14:27:19 +0000

    fixed rel imports of exp01 and 02

--- micro_scripts/start_docker_3.2.1.sh
+++ micro_scripts/start_docker_3.2.1.sh
@@ -5,8 +5,8 @@ docker run -it --entrypoint /bin/bash \
     -v $parent/experiments/:/home/dev/experiments \
     -v $parent/root.qcow2:/home/dev/root.qcow2 \
     -v $parent/qflex/micro_scripts:/home/dev/qflex/micro_scripts \
-    -v $parent/exp02:/home/dev/exp02 \
-    -v $parent/exp01:/home/dev/exp01 \
+    -v $parent/../exp02:/home/dev/exp02 \
+    -v $parent/../exp01:/home/dev/exp01 \
     -w /home/dev/qflex/micro_scripts \
     --security-opt seccomp=unconfined \
     --cap-add SYS_PTRACE \
