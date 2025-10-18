54afe49482ecda9fc8358e0d7fef9ae19b24efcb
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-10-18 04:22:59 +0000

    added copy for core_info_new.csv

--- micro_scripts/result.py
+++ micro_scripts/result.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 
 
 if __name__ == "__main__":
@@ -10,6 +9,7 @@ if __name__ == "__main__":
     full_command = " && ".join([
         "cd ..",
         command,
+        "cp /home/dev/experiments/experiment_micro/cfg/core_info.csv /home/dev/qflex/micro_scripts/core_info_new.csv"
     ])
     os.system(full_command)
 
