494128c94f9c06982b5f977abc74a3bb05dc40ad
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-10-18 01:41:01 +0000

    swapped start_docker files

--- micro_scripts/start_docker.sh
+++ micro_scripts/start_docker.sh
@@ -1,5 +1,21 @@
 pushd ..
 
-./dep start-docker --worm --mounting-folder /mnt/sdb/exp02 --start-directory /home/dev/qflex/micro_scripts
+parent=$(realpath $PWD/..)
+docker run -it --entrypoint /bin/bash \
+    -v $parent/experiments/:/home/dev/experiments \
+    -v $parent/root.qcow2:/home/dev/root.qcow2 \
+    -v $parent/qflex/micro_scripts:/home/dev/qflex/micro_scripts \
+    -v $parent/../exp02:/exp02 \
+    -v $parent/../exp01:/exp01 \
+    -v $parent/qflex/micro_scripts:/home/dev/qflex/micro_scripts \
+    -v $parent/qflex/commands:/home/dev/qflex/commands \
+    -w /home/dev/qflex/micro_scripts \
+    --security-opt seccomp=unconfined \
+    --cap-add SYS_PTRACE \
+    ghcr.io/parsa-epfl/qflex:qflex-worm-release-3.2.1 -c "rm -rf /exp01/lib && mkdir -p /exp01/lib && cp /home/dev/qflex/kraken_out/* /exp01/lib && rm -rf /exp01/lib && mkdir -p /exp01/lib && cp /home/dev/qflex/kraken_out/* /exp01/lib  && bash"
+
+
 
 popd
+
+ 
