d4af2ad61702eaad3afafd4a8724ea379c1071dc
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-10-18 03:02:28 +0000

    added mounting qflex binary for micro tutorial

--- micro_scripts/start_docker.sh
+++ micro_scripts/start_docker.sh
@@ -16,6 +16,7 @@ docker run -it --entrypoint /bin/bash \
     -v $parent/../exp01:/exp01 \
     -v $parent/qflex/micro_scripts:/home/dev/qflex/micro_scripts \
     -v $parent/qflex/commands:/home/dev/qflex/commands \
+    -v $parent/qflex/qflex:/home/dev/qflex/qflex \
     -w /home/dev/qflex/micro_scripts \
     --security-opt seccomp=unconfined \
     --cap-add SYS_PTRACE \
