e0869e54f1542ec90f8ec59e1ecda3d6e0b9c7ac
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-10-18 00:45:02 +0000

    added link files after boot

--- micro_scripts/boot.sh
+++ micro_scripts/boot.sh
@@ -2,4 +2,10 @@ pushd ..
 
 xargs -a ./micro_scripts/qflex.micro.args -- ./qflex boot
 
+# folders "bin", "cfg", "flags", "lib", "run", "scripts", "images" are in the experiment folder, linking them for accesibility during the presentation
+for folder in bin cfg flags lib run scripts images; do
+    unlink ./micro_scripts/$folder > /dev/null 2>&1
+    ln -s $PWD/../experiments/experiment_micro/$folder ./micro_scripts/$folder
+done
+
 popd
