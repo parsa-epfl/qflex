e57aa30f7aafe961d096f41a0d3a4589175bcea2
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-11-11 22:29:10 +0000

    added quantum time with small quantum

--- commands/multinode.py
+++ commands/multinode.py
@@ -17,7 +17,8 @@ class Multi(Executor):
         
         boot_cmd = f"""
         ./qemu-system-aarch64 \
-        {self.qemu_common_parser.get_qemu_base_args()}
+        {self.qemu_common_parser.get_qemu_base_args()} \
+        {self.qemu_common_parser.quantum_args()}
         """
 
         print(f"{boot_cmd}")
