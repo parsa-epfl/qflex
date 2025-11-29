d63108a80a7cf826e8c74e9174c2a94d83fb43da
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2025-11-29 18:39:28 +0000

    icount also working

--- commands/multinode.py
+++ commands/multinode.py
@@ -17,7 +17,8 @@ class Multi(Executor):
         
         boot_cmd = f"""
         ./qemu-system-aarch64 \
-        {self.qemu_common_parser.get_qemu_base_args()}
+        {self.qemu_common_parser.get_qemu_base_args()} \
+        -icount shift=0,align=off,sleep=off
         """
         # -icount shift=0,align=off,sleep=off
 
